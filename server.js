03916117d799a16a386c8c5e05016d0ba8818970
ivanmironov1235 <ivan.mironov1235@gmail.com>
2026-02-24 01:40:59 +0500

    Update server.js

--- server.js
+++ server.js
@@ -135,3 +135,10 @@ app.post("/update-bg", (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`VibeHub online at ${PORT}`));
+
+const fs = require("fs");
+
+if (!fs.existsSync("uploads")) {
+  fs.mkdirSync("uploads");
+  console.log("Создана папка uploads");
+}
